979507d1130e23f77e1f63dfbd101373c1443adc
LeeCharry <13155535991@163.com>
2016-11-10 11:01:55 +0800

    添加log

--- app/src/main/java/com/example/admin/hday14_git/MainActivity.java
+++ app/src/main/java/com/example/admin/hday14_git/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onCreate: 111111111");
 
         Log.i(TAG, "onCreate: ");
+
+
         Log.i(TAG, "onCreate: 2222222222222");
     }
 }
